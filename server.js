7d45e3f27a2b5e61c8100d93173ca14114193189
sameerasad <ssameerkhan525@gmail.com>
2021-02-03 17:53:46 +0500

    connection of mongodb atlas

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const connectDb = require("./config/db");
 const app = express();
 
-app.get("/", (req, res) => res.send(API));
+//connect database
+
+connectDb();
+
+app.get("/", (req, res) => res.send("API running"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server listening at ${port}`));
