61b6653483f0d2a46057d3c0180be9080c5e6413
sameerasad <ssameerkhan525@gmail.com>
2021-02-11 21:53:52 +0500

    user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const { check, validationResult } = require("express-validator");
 const user = require("../../models/User");
 const gravatar = require("gravatar");
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
@@ -31,7 +33,27 @@ router.post(
         res.status(400).json({ errors: [{ msg: "user already exist" }] });
       }
 
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+      //create instance of user in db
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+      //password is not hashed yet
+      //use bcrypt to hashed the password
+
+      const salt = await bcrypt.genSalt(10); // bcrypt return a promise
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
       res.json({ msg: "Users Works" });
+      res.send("User Registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("user exist");
