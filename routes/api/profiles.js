853edfa003ca31c2e57e6e9a8ee03746f1fa9598
sameerasad <ssameerkhan525@gmail.com>
2021-02-17 14:59:23 +0500

    get current user profile

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -1,12 +1,30 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const auth = require("../../middleware/auth");
 const router = express.Router();
+// Load Profile Model
+const Profile = require("../../models/Profile");
+// Load User Model
+const User = require("../../models/User");
 
-//@route Get api/profiles
-//@desc  test route
-//acsess public
-router.get("/", (req, res) => {
-  res.json({ msg: "Profiles works" });
+//@route Get api/profiles/me
+//@desc  test routes
+//acsess private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populated("user", ["name", "avatar"]);
+
+    if (!profile) {
+      res.status(400).json({ msg: "there is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    res.status(500).send("server Error");
+  }
 });
 
 module.exports = router;
