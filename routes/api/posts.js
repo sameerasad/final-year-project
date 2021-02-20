2f833842abe2a3ff7b98302e7b383dc94566ac67
sameerasad <ssameerkhan525@gmail.com>
2021-02-20 14:07:26 +0500

    put a like of Post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -109,4 +109,27 @@ router.delete("/:id", auth, async (req, res) => {
   }
 });
 
+//@route  Put api/posts/like/:id
+//@desc  like a post
+//acsess private
+
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+    post.likes.unshift(req.user.id);
+
+    await post.save();
+    res.json(likes);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
