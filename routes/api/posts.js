2599499f8d16794a6a0f1c0a0328521b28b225e6
sameerasad <ssameerkhan525@gmail.com>
2021-02-20 21:14:12 +0500

    add and remove comments Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
 const Post = require("../../models/Post");
 const Profile = require("../../models/Profile");
+const { findById } = require("../../models/User");
 
 //@route post api/post
 //@desc  create a post
@@ -23,12 +24,12 @@ router.post(
     try {
       const user = User.findById(req.user.id).select("-password");
 
-      const newPost = {
+      const newPost = new Post({
         text: req.body.text,
         name: req.user.name,
         avatar: req.body.avatar,
         user: req.user.id,
-      };
+      });
 
       const post = await newPost.save();
       res.json(post);
@@ -125,7 +126,7 @@ router.put("/like/:id", auth, async (req, res) => {
     post.likes.unshift({ user: req.user.id });
 
     await post.save();
-    res.json(likes);
+    res.json(post.likes);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
@@ -147,17 +148,92 @@ router.put("/unlike/:id", auth, async (req, res) => {
     }
     // remove index
 
-    const removeIndex = posts.like
+    const removeIndex = post.likes
       .map((like) => like.user.toString())
-      .indexOff(req.user.id);
+      .indexOf(req.user.id);
 
     post.like.splice(removeIndex, 1);
 
     await post.save();
-    res.json(likes);
+    res.json(post.likes);
   } catch (error) {
     console.error(error);
     res.status(500).send("Server Error");
+  }
+});
+
+//@route post api/comment/:id
+//@desc  create a comment
+//acsess private
+router.post(
+  "/comment/:id",
+  [auth, [check("text", "text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = User.findById(req.user.id).select("-password");
+      const post = findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: req.user.name,
+        avatar: req.body.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+      await post.save();
+
+      res.json(post.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("server Error");
+    }
+  }
+);
+//@route Delete api/comment/:id/comment_id
+//@desc  Delete a comment
+//acsess private
+
+router.delete("/comment/:id/comment_id", auth, async (req, res) => {
+  try {
+    const post = findById(req.params.id);
+
+    // pull out comment
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // make sure comment exist
+
+    if (!comment) return res.status(404).json({ msg: "Bad Request" });
+
+    // check user
+
+    if (!comment.user.toString() === req.user.id) {
+      return res.status(401).json({ msg: "unauthorized user" });
+    }
+    // get Remove Index
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    //save post which include comment as well
+    await post.save();
+
+    // send respons
+    res.json(post.comments);
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Server Error");
   }
 });
 
